aa5b06c48cf00512c5798ea1a9dfaf0e61e4f4d2
arpitgupta1906 <gupta.25.iitj.ac.in>
2019-11-16 19:54:07 +0530

    created smoothing function

---
+++ smoothing.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def smoothing(wav_data):
+    """ parameter wavedata"""
+    r = 3
+    wav = np.zeros(len(wav_data))
+    for i in range(r, len(wav) - r):
+        wav[i] = sum(wav_data[i - r:i + r + 1]) / (2 * r + 1)
+
+    return wav
