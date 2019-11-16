69a7ef42e0caae8b8ce93426ab338073853f61f4
Tanuj22 <agarwal.5@iitj.ac.in>
2019-11-16 14:17:01 +0530

    made seperate files of ploy.py

--- mappingRegression.py
+++ mappingRegression.py
@@ -7,124 +7,119 @@ import wave
 from pylab import *
 from scipy.io import wavfile
 
-def plot(data):
-    plt.plot(data, color ='steelblue')
 
-#function to smooth an audio signal
+def plot(data):
+    plt.plot(data, color='steelblue')
+
+
+# function to smooth an audio signal
 def smoothing(wav_data):
-    r=3
-    wav=np.zeros(len(wav_data))
-    for i in range(r,len(wav)-r):
-        wav[i]=sum(wav_data[i-r:i+r+1])/(2*r+1)
-    
+    r = 3
+    wav = np.zeros(len(wav_data))
+    for i in range(r, len(wav) - r):
+        wav[i] = sum(wav_data[i - r:i + r + 1]) / (2 * r + 1)
+
     return wav
 
 
-#function to return the regression coefficients
+# function to return the regression coefficients
 def regressionCoefficients(wav_data):
-    #smoothen the signal
-    wav1=smoothing(wav_data)
+    # smoothen the signal
+    wav1 = smoothing(wav_data)
 
-    L1=len(wav1)
+    L1 = len(wav1)
 
-    fft1=np.linspace(0,8000,L1)
-    f1=np.linspace(0,4000,L1/2)
+    fft1 = np.linspace(0, 8000, L1)
+    f1 = np.linspace(0, 4000, L1 / 2)
 
-    xx1=np.fft.fft(wav1)
-    xx1=np.abs(real(xx1))/L1
+    xx1 = np.fft.fft(wav1)
+    xx1 = np.abs(real(xx1)) / L1
 
-    x1=2*xx1[0:L1/2]
+    x1 = 2 * xx1[0:L1 / 2]
 
-    max1=max(x1)
+    max1 = max(x1)
 
-    x1=2*xx1[0:L1/2]
-    max1=max(x1)
+    x1 = 2 * xx1[0:L1 / 2]
+    max1 = max(x1)
 
-    #find peaks above threshold of 20%
-    peaks1=findpeaks(x1,50,.2)  
-    plt.plot(f1,x1)
+    # find peaks above threshold of 20%
+    peaks1 = findpeaks(x1, 50, .2)
+    plt.plot(f1, x1)
 
-    #plot peaks
-    plt.plot(f1[peaks1],x1[peaks1],'ro')
+    # plot peaks
+    plt.plot(f1[peaks1], x1[peaks1], 'ro')
     plt.show()
-    freq1=f1[peaks1]
+    freq1 = f1[peaks1]
 
-    #Regression Coefficients
-    coef1=x1[peaks1]*max1
+    # Regression Coefficients
+    coef1 = x1[peaks1] * max1
 
     return coef1
 
 
 # input in waves here
-FILENAME1 ="output1.wav"
-FILENAME2="output2.wav"
-FILENAME3="output3.wav"
+FILENAME1 = "output1.wav"
+FILENAME2 = "output2.wav"
+FILENAME3 = "output3.wav"
 
+rate1, wav_data1 = wavfile.read(FILENAME1)
+rate2, wav_data2 = wavfile.read(FILENAME2)
+rate3, wav_data3 = wavfile.read(FILENAME3)
 
-rate1,wav_data1=wavfile.read(FILENAME1)
-rate2,wav_data2=wavfile.read(FILENAME2)
-rate3,wav_data3=wavfile.read(FILENAME3)
+# coeficients of regression
+coef1 = regressionCoefficients(wav_data1)
+coef2 = regressionCoefficients(wav_data2)
 
-#coeficients of regression
-coef1=regressionCoefficients(wav_data1)
-coef2=regressionCoefficients(wav_data2)
+# include end points of regression
+n = len(coef1)
+C1 = np.zeros(n + 2)
+C1[1:n + 1] = coef1
+C1[n + 1] = 1.2 * C1[n]
+C2 = np.zeros(n + 2)
+C2[1:n + 1] = coef2
+C2[n + 1] = 1.2 * C2[n]
 
-#include end points of regression
-n=len(coef1)
-C1=np.zeros(n+2)
-C1[1:n+1]=coef1
-C1[n+1]=1.2*C1[n]
-C2=np.zeros(n+2)
-C2[1:n+1]=coef2
-C2[n+1]=1.2*C2[n]
+# plot coefficients
+plt.plot(C1, C2, 'o')
 
-#plot coefficients
-plt.plot(C1,C2,'o')
+# Construct polynomial of order n-1
+X = np.asmatrix(C1)
+Y = np.asmatrix(C2)
+b = np.transpose(Y)
 
-#Construct polynomial of order n-1
-X=np.asmatrix(C1)
-Y=np.asmatrix(C2)
-b=np.transpose(Y)
+A = np.zeros([n, n])
+A = np.asmatrix(A)
 
-A=np.zeros([n,n])
-A=np.asmatrix(A)
+for i in range(0, n):
+    for j in range(0, n):
+        A[i, j] = X[0, i] ** (n - j - 1)
+coef = A ** (-1) * b
 
-for i in range(0,n):
-    for j in range(0,n):
-        A[i,j]=X[0,i]**(n-j-1)
-coef=A**(-1)*b
+# plot best fit line to coefficients
+xaxis = np.linspace(0, C1[-1], 1000)
+yaxis = np.linspace(0, 0, 1000)
 
-
-#plot best fit line to coefficients
-xaxis=np.linspace(0,C1[-1],1000)
-yaxis=np.linspace(0,0,1000)
-
-for i in range(0,1000):
+for i in range(0, 1000):
     for j in range(n):
-        yaxis[i]+=coef[j]*xaxis[i]**(n-j-1)
+        yaxis[i] += coef[j] * xaxis[i] ** (n - j - 1)
 
-plt.plot(X,Y,'ro')
-plt.plot(xaxis,yaxis)
+plt.plot(X, Y, 'ro')
+plt.plot(xaxis, yaxis)
 
+# find FFT of third audio sample
+L3 = len(wav_data3)
+ff3 = np.linspace(0, 8000, L3)
+f3 = np.linspace(0, 4000, L3 / 2)
+xx3 = np.fft.fft(wav_data3)
+xx3 = np.abs(real(xx3)) / L3
+x3 = 2 * xx3[0:L3 / 2]
 
-#find FFT of third audio sample
-L3=len(wav_data3)
-ff3=np.linspace(0,8000,L3)
-f3=np.linspace(0,4000,L3/2)
-xx3=np.fft.fft(wav_data3)
-xx3=np.abs(real(xx3))/L3
-x3=2*xx3[0:L3/2]
-
-
-#push coefficients through regression model found from vector "coef"
-n=len(coef)
-ynew=np.linspace(0,0,L3/2)
-for i in range(L3/2):
+# push coefficients through regression model found from vector "coef"
+n = len(coef)
+ynew = np.linspace(0, 0, L3 / 2)
+for i in range(L3 / 2):
     for j in range(n):
-        ynew[i]+=coef[j]*x3[i]**(n-j-1)
+        ynew[i] += coef[j] * x3[i] ** (n - j - 1)
 
-#plot the transformation of the coefficients
-plt.plot(f3,yaxis)
-
-
-
+# plot the transformation of the coefficients
+plt.plot(f3, yaxis)
--- sacalarmapping.py
+++ sacalarmapping.py
@@ -1,53 +1,52 @@
 import matplotlib.pyplot as plt
 import numpy as np
-#from findpeaks import findpeaks
+# from findpeaks import findpeaks
 import pyaudio
 import wave
 from pylab import *
 from scipy.io import wavfile
 
-def plot(data):
-    plt.plot(data, color ='steelblue')
 
-#Scalar multiplication by ratio of magnitudes
+def plot(data):
+    plt.plot(data, color='steelblue')
+
+
+# Scalar multiplication by ratio of magnitudes
 # in dono file ka naam dekhna hai code me sirf example leke btaya hai
 FILENAME1 = "input1.wav"
-FILENAME2 =  "output1.wav"
+FILENAME2 = "output1.wav"
 FILENAME3 = "third1.wav"
-rate1 , wav_data1 = wavfile.read(FILENAME1)
-rate2 , wav_data2 = wavfile.read(FILENAME2) #isko dekhna hai
-rate3 , wav_data3 = wavfile.read(FILENAME3)
-
-
-
+rate1, wav_data1 = wavfile.read(FILENAME1)
+rate2, wav_data2 = wavfile.read(FILENAME2)  # isko dekhna hai
+rate3, wav_data3 = wavfile.read(FILENAME3)
 
 L1 = len(wav_data1)
-f1 = np.linspace(0,8000,L1)
-x1=np.fft.fft(wav_data1)
-X1 = np.abs(x1)/L1  # magnitude of coefficient of FFT of source voice
+f1 = np.linspace(0, 8000, L1)
+x1 = np.fft.fft(wav_data1)
+X1 = np.abs(x1) / L1  # magnitude of coefficient of FFT of source voice
 L2 = len(wav_data2)
-f2 = np.linspace(0,8000,L2)
+f2 = np.linspace(0, 8000, L2)
 x2 = np.fft.fft(wav_data2)
-X2 = np.abs(x2)/L2    # magnitude of  coefficient of FFT of target voice to be replicated
-ratio = abs(x2)/abs(x1)   #ratio of coefficients r=d/c 
-Freq = np.linspace(0,8000,L1)
-plt.plot(Freq,ratio) #plot ratio over double sided FFT spectrum
+X2 = np.abs(x2) / L2  # magnitude of  coefficient of FFT of target voice to be replicated
+ratio = abs(x2) / abs(x1)  # ratio of coefficients r=d/c
+Freq = np.linspace(0, 8000, L1)
+plt.plot(Freq, ratio)  # plot ratio over double sided FFT spectrum
 
-#This will help us cut down on the ratio .
-#You need to pick values of h>1.
-#The closer to 1 , the more it shrinks
-#The farther h is from 1 , a the smaller the reduction
+# This will help us cut down on the ratio .
+# You need to pick values of h>1.
+# The closer to 1 , the more it shrinks
+# The farther h is from 1 , a the smaller the reduction
 
 h = 1.25
 Ratio = np.zeros(len(ratio))
 
 for i in range(len(ratio)):
-    Ratio[i]=((h-1)*ratio[i]+1)/h
+    Ratio[i] = ((h - 1) * ratio[i] + 1) / h
 plt.plot(Freq.Ratio)
 
-#Apply ratio multipilcation on third audio sample
+# Apply ratio multipilcation on third audio sample
 
 L3 = len(wav_data3)
-f3 = np.linspace(0,8000,L3)
+f3 = np.linspace(0, 8000, L3)
 x3 = np.fft.fft(wav_data3)
-x3 = x3*Ratio    
+x3 = x3 * Ratio
