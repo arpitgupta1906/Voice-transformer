73b58dd315cdb8b71436351f68c334f1f77ba0cd
arpitgupta1906 <gupta.25.iitj.ac.in>
2019-11-25 01:29:43 +0530

    zoomed images

--- sacalarmapping.py
+++ sacalarmapping.py
@@ -28,48 +28,51 @@ rate1, wav_data1 = wavfile.read(FILENAME1)
 rate2, wav_data2 = wavfile.read(FILENAME2)  # isko dekhna hai
 rate3, wav_data3 = wavfile.read(FILENAME3)
 
+plt.plot(wav_data1)
+plt.show()
+
 wav1 = smoothing(wav_data1)
 wav2 = smoothing(wav_data2)
 wav3 = smoothing(wav_data3)
 
 
-L1 = len(wav1)
-f1 = np.linspace(0, 8000, L1)
-x1 = np.fft.fft(wav1)
-X1 = np.abs(x1) / L1  # magnitude of coefficient of FFT of source voice
-L2 = len(wav2)
-f2 = np.linspace(0, 8000, L2)
-x2 = np.fft.fft(wav2)
-X2 = np.abs(x2) / L2  # magnitude of  coefficient of FFT of target voice to be replicated
-ratio = abs(x2) / abs(x1)  # ratio of coefficients r=d/c
-Freq = np.linspace(0, 8000, L1)
-plt.plot(Freq, ratio)  # plot ratio over double sided FFT spectrum
+# L1 = len(wav1)
+# f1 = np.linspace(0, 8000, L1)
+# x1 = np.fft.fft(wav1)
+# X1 = np.abs(x1) / L1  # magnitude of coefficient of FFT of source voice
+# L2 = len(wav2)
+# f2 = np.linspace(0, 8000, L2)
+# x2 = np.fft.fft(wav2)
+# X2 = np.abs(x2) / L2  # magnitude of  coefficient of FFT of target voice to be replicated
+# ratio = abs(x2) / abs(x1)  # ratio of coefficients r=d/c
+# Freq = np.linspace(0, 8000, L1)
+# plt.plot(Freq, ratio)  # plot ratio over double sided FFT spectrum
 
-# This will help us cut down on the ratio .
-# You need to pick values of h>1.
-# The closer to 1 , the more it shrinks
-# The farther h is from 1 , a the smaller the reduction
+# # This will help us cut down on the ratio .
+# # You need to pick values of h>1.
+# # The closer to 1 , the more it shrinks
+# # The farther h is from 1 , a the smaller the reduction
 
-h = 1.25
-Ratio = np.zeros(len(ratio))
+# h = 1.25
+# Ratio = np.zeros(len(ratio))
 
-for i in range(len(ratio)):
-    Ratio[i] = ((h - 1) * ratio[i] + 1) / h
-plt.plot(Freq , Ratio)
-# plt.show()
+# for i in range(len(ratio)):
+#     Ratio[i] = ((h - 1) * ratio[i] + 1) / h
+# plt.plot(Freq , Ratio)
+# # plt.show()
 
-# Apply ratio multipilcation on third audio sample
+# # Apply ratio multipilcation on third audio sample
 
-L3 = len(wav3)
-f3 = np.linspace(0, 8000, L3)
-x3 = np.fft.fft(wav3)
-x3 = x3 * Ratio
-ifftplot=np.fft.ifft(x3)
+# L3 = len(wav3)
+# f3 = np.linspace(0, 8000, L3)
+# x3 = np.fft.fft(wav3)
+# x3 = x3 * Ratio
+# ifftplot=np.fft.ifft(x3)
+# # ifftplot=smoothing(ifftplot)
+# filteredwrite=np.round(ifftplot).astype('int16')
+# wavfile.write('pleasework.wav', rate3, filteredwrite)
 # ifftplot=smoothing(ifftplot)
-filteredwrite=np.round(ifftplot).astype('int16')
-wavfile.write('pleasework.wav', rate3, filteredwrite)
-ifftplot=smoothing(ifftplot)
 
-plt.plot(ifftplot)
-plt.show()
+# plt.plot(ifftplot)
+# plt.show()
 
